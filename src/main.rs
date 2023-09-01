5ffa7cdcac9a0365e774a9e739b034ae0e7d0601
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-09-02 01:32:54 +0800

    include wordlist with binary instead of reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
 mod game_manager;
 
-use std::io;
-use std::fs;
-use std::io::Write;
+use std::io::{self, Write};
 use std::process::exit;
 
 use rand::seq::SliceRandom;
 
 use crate::game_manager::*;
 
-const WORDLIST: &'static str = "src/wordlist.txt";
+const WORDLIST_CONTENTS: &'static str = include_str!("wordlist.txt");
 
 fn main() {
     println!("Hangman v1.0 by sertdfyguhi.");
 
     // reads wordlist
-    println!("Reading wordlist...");
-    let contents = match fs::read_to_string(WORDLIST) {
-        Ok(contents) => contents,
-        Err(err) => {
-            eprintln!("Error when reading wordlist: {:?}", err);
-            exit(1)
-        }
-    };
-    let wordlist: Vec<&str> = contents.split('\n').collect();
+    println!("Loading wordlist...");
+    let wordlist: Vec<&str> = WORDLIST_CONTENTS.split('\n').collect();
 
     if wordlist.len() == 0 {
         println!("Empty wordlist found.");
